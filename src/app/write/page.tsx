e45b9258d75a3f88ac6860bb5d918022cb147622
adetimmy <ogunleademola@gmail.com>
2023-09-22 04:19:34 +0100

    rounding up; 80% done

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -19,13 +19,19 @@ const WritePage = () => {
 
 
   const [file, setFile] = React.useState<null | any>(null) 
+  const [media, setMedia] = React.useState<string>('') 
   const [open, setOpen] = React.useState(false)
   const [value, setValue] = React.useState('')
+  const [title, setTitle] = React.useState('')
+  const [catSlug, setCatSlug] = React.useState("");
+
+
 
   React.useEffect(() => {
     const upload = () => {
-
-const storageRef = ref(storage, file.name);
+      // to create a unique file
+      const name = new Date().getTime() + file.name 
+const storageRef = ref(storage, name);
 
 const uploadTask = uploadBytesResumable(storageRef, file);
 
@@ -55,13 +61,13 @@ uploadTask.on('state_changed',
     // Handle successful uploads on complete
     // For instance, get the download URL: https://firebasestorage.googleapis.com/...
     getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-      console.log('File available at', downloadURL);
+      setMedia(downloadURL)
     });
   }
 );
     }
 
-    file && upload
+    file && upload()
   }, [file])
 
 if (status === "loading"){
@@ -72,9 +78,46 @@ else if(status === 'unauthenticated'){
   return router.push('/')
 }
 
+const slugify = (str:string) =>
+str
+  .toLowerCase()
+  .trim()
+  .replace(/[^\w\s-]/g, "")
+  .replace(/[\s_-]+/g, "-")
+  .replace(/^-+|-+$/g, "");
+
+const handleSubmit = async () => {
+const res = await fetch("/api/posts", {
+  method: "POST",
+  body: JSON.stringify({
+    title,
+    desc: value,
+    img: media,
+    slug: slugify(title),
+    catSlug: catSlug || "style", //If not selected, choose the general category
+  }),
+});
+
+if (res.status === 200) {
+  const data = await res.json();
+  router.push(`/posts/${data.slug}`);
+}
+};
+
+
   return (
     <div className='writ'>
-      <input type="text" placeholder='Title' className='p-12 text-[64px]  outline-none placeholder:text-[#b3b3b1] ' />
+      <input type="text" placeholder='Title' className='p-12 text-[64px]  outline-none placeholder:text-[#b3b3b1] ' value={title} onChange={e => setTitle(e.currentTarget.value)} />
+
+      <select className='mt-10 px-5 py-3 ml-10 w-max' onChange={(e) => setCatSlug(e.target.value)}>
+        <option value="style">style</option>
+        <option value="fashion">fashion</option>
+        <option value="food">food</option>
+        <option value="culture">culture</option>
+        <option value="travel">travel</option>
+        <option value="coding">coding</option>
+      </select>
+
       <div className='flex write gap-5 h-[700px] relative'>
         <button type="button" onClick={() => setOpen(!open)}>
           <Image src='/plus.png' alt='plus' width={16} height={16} className=''/>
@@ -95,9 +138,10 @@ else if(status === 'unauthenticated'){
             </button>
           </div>
         }
-        <ReactQuill theme='bubble' value={value} onChange={(e:any) => setValue(e.target.value)} placeholder='Tell your story....' className='w-full'/>
+
+        <ReactQuill theme='bubble' value={value} onChange={setValue} placeholder='Tell your story....' className='w-full'/>
       </div>
-      <button className='absolute right-5 top-7 text-sm px-5 py-2 bg-[#1a8917] text-[white] rounded-2xl '>Publish</button>
+      <button className='absolute right-5 top-7 text-sm px-5 py-2 bg-[#1a8917] text-[white] rounded-2xl' onClick={handleSubmit}>Publish</button>
    </div>
   )
 }
